ca9430280da842277c3b8261dd907e67af8e7d87
Ivo-Schwizer <ivo@schwizerschlatter.com>
2020-04-28 17:00:30 +0200

    icons

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     
     <?php include 'header.php'; ?>
     <div><h1 class="h1red">Alltagsmaske</h1></div>
-    <section class="sectionhome">
+    <section>
         <article> 
             <h1>Sie suchen den f&uuml;r Sie passenden Schutz gegen Covid-19. Dazu sollten Sie die drei bekannten Haupt&uuml;bertragungswege des neuen Corona-Virus kennen:</h1>
             <figure class="threeicon">
@@ -27,6 +27,26 @@
                 <img src="img/alltagsmasken-covid-19-hygienemasken-virenschutzscheibe-haende.svg" alt="alltagsmasken-covid-19-hygienemasken-virenschutzscheibe-haende" style="width:100%">
                 <figcaption><span>H&auml;nde:</span><br> Ansteckende Tr&ouml;pfchen gelangen beim Husten und Niesen oder Ber&uuml;hren der Schleimh&auml;ute auf die H&auml;nde. Von da aus werden die Viren auf Oberfl&auml;chen &uuml;bertragen. Eine andere Person kann von da aus die Viren auf ihre H&auml;nde &uuml;bertragen und so gelangen sie an Mund, Nase oder Augen, wenn man sich ins Gesicht fasst.</figcaption>
             </figure>
+            <p>Wenn Sie sich im Alltag in jeder Situation zu 100 % an die Verhaltens- und Hygieneregeln des Bundesamtes für Gesundheit BAG halten können brauchen Sie keinen Schutz für Augen, Mund und Nase. Kaufen Sie kein Produkt, wenn sie keines brauchen.</p>
+        </article>    
+    </section>
+    
+    <section>
+        <article> 
+            <h1>Mit diesen Produkten können Sie sich von den Tröpfchen schützen!</h1>
+            <figure class="threeicon">
+                <img src="img/alltagsmasken-covid-19-hygienemasken-virenschutzscheibe-neu.svg" alt="alltagsmasken-covid-19-hygienemasken-virenschutzscheibe-neu" style="width:100%">
+                <figcaption><span>Enger Kontakt:</span><br> Wenn man zu einer erkrankten Person weniger als zwei Meter Abstand h&auml;lt.</figcaption>
+            </figure>
+            <figure class="threeicon">
+                <img src="img/alltagsmasken-covid-19-hygienemasken.svg" alt="alltagsmasken-covid-19-hygienemasken" style="width:100%">
+                <figcaption><span>Tr&ouml;pfchen:</span><br> Niest oder hustet eine erkrankte Person, k&ouml;nnen die Viren direkt auf die Schleimh&auml;ute von Nase, Mund oder Augen eines anderen Menschen gelangen.</figcaption>
+            </figure>
+            <figure class="threeicon r">
+                <img src="img/alltagsmasken-covid-19-atemschutzmaske-ffp2.svg" alt="alltagsmasken-covid-19-atemschutzmaske-ffp2" style="width:100%">
+                <figcaption><span>H&auml;nde:</span><br> Ansteckende Tr&ouml;pfchen gelangen beim Husten und Niesen oder Ber&uuml;hren der Schleimh&auml;ute auf die H&auml;nde. Von da aus werden die Viren auf Oberfl&auml;chen &uuml;bertragen. Eine andere Person kann von da aus die Viren auf ihre H&auml;nde &uuml;bertragen und so gelangen sie an Mund, Nase oder Augen, wenn man sich ins Gesicht fasst.</figcaption>
+            </figure>
+            <p>Wenn Sie sich im Alltag in jeder Situation zu 100 % an die Verhaltens- und Hygieneregeln des Bundesamtes für Gesundheit BAG halten können brauchen Sie keinen Schutz für Augen, Mund und Nase. Kaufen Sie kein Produkt, wenn sie keines brauchen.</p>
         </article>    
     </section>
     
