8789b5391fe41e6e30785fff97e698b44f89c6f7
timbukflow <ivoschwizer@gmail.com>
2020-11-24 16:14:39 +0100

    update preise

--- einweg-hygienemaske.php
+++ einweg-hygienemaske.php
@@ -8,14 +8,14 @@
 Hier finden Sie ihn."/>
 <?php include 'head.php'; ?>
 </head>
-    
+
 <body>
     <?php include 'header.php'; ?>
     <div>
         <h1 class="h1red">Einweg-Hygienemasken</h1>
     </div>
     <section>
-        <article class="maskencontainer"> 
+        <article class="maskencontainer">
             <h1>Hygienemaske Typ I</h1>
             <div class="twoicon">
                 <img src="img/alltagsmasken-covid-19-hygienemaske.jpg" alt="alltagsmasken-covid-19-hygienemaske" style="width:100%">
@@ -26,57 +26,57 @@ Hier finden Sie ihn."/>
                 <p class="wichtig"><b>Bakterienfiltrationseffizienz BFE ≥96 %</b></p>
                 <ul>
                     <li class="menge">Bestellmenge/Stück<span>Preis</span></li>
-                    <li class="title">2’000<span>Fr. 0.38</span></li>
-                    <li class="title">6’000<span>Fr. 0.35</span></li>
-                    <li class="title">10’000<span>Fr. 0.33</span></li>
-                    <li class="title">32’000<span>Fr. 0.31</span></li>
-                    <li class="title">192’000<span>Fr. 0.29</span></li>
+                    <li class="title">1’000<span>Fr. 0.28</span></li>
+                    <li class="title">3’000<span>Fr. 0.27</span></li>
+                    <li class="title">5’000<span>Fr. 0.26</span></li>
+                    <li class="title">10’000<span>Fr. 0.25</span></li>
+                    <li class="title">42’000<span>Fr. 0.24</span></li>
                 </ul>
-                <p class="preiszusatz">Verpackung: Box à 50 Stück<br>Preise: in CHF, zuzüglich Porto und 7,7 % MWST</p>
+                <p class="preiszusatz">Verpackung: 2x 25 Stück in 50er Box, 1'000 Stück pro Karton<br>Preise: in CHF, zuzüglich Porto und 7,7 % MWST</p>
             </div>
         </article>
-        <article class="maskencontainer"> 
-            <h1>Hygienemaske Typ II</h1>
+        <article class="maskencontainer">
+            <h1>Hygienemaske Typ IIR</h1>
             <div class="twoicon">
                 <img src="img/alltagsmasken-covid-19-hygienemaske-typ-2.jpg" alt="alltagsmasken-covid-19-hygienemaske-typ-2" style="width:100%">
                 <h1 class="productb">Sofort lieferbar!</h1>
             </div>
             <div class="twoicon r">
-                <p class="wichtig">Die 3-lagigen Masken Typ II sind CE- und FDA-zertifiziert. Sie sind für den alltäglichen Gebrauch bestimmt.<br><br> Das Bundesamt für Gesundheit empfiehlt die Verwendung dieser Qualität für Gesundheitspersonal sowie für Alters- und Pflegeheime. Gemäss Pandemieplan rät das BAG jeder Privatperson einen Notvorrat von 50 solcher Masken anzulegen.</p>
-                <p class="wichtig"><b>Bakterienfiltrationseffizienz BFE ≥98 %</b></p>
-            
+                <p class="wichtig">Die sterilen 3-lagigen Masken Typ IIR sind CE- und FDA-zertifiziert. Sie sind für den alltäglichen Gebrauch bestimmt.<br><br> Das Bundesamt für Gesundheit empfiehlt die Verwendung dieser Qualität für Gesundheitspersonal sowie für Alters- und Pflegeheime. Gemäss Pandemieplan rät das BAG jeder Privatperson einen Notvorrat von 50 solcher Masken anzulegen.</p>
+                <p class="wichtig"><b>Bakterienfiltrationseffizienz BFE ≥98 %<br>Zertifizierung EN 14683:2019</b></p>
+
                 <ul>
                     <li class="menge">Bestellmenge/Stück<span>Preis</span></li>
-                    <li class="title">2’000<span>Fr. 1.10</span></li>
-                    <li class="title">6’000<span>Fr. 0.97</span></li>
-                    <li class="title">10’000<span>Fr. 0.92</span></li>
-                    <li class="title">36’000<span>Fr. 0.85</span></li>
+                    <li class="title">1’000<span>Fr. 0.43</span></li>
+                    <li class="title">3’000<span>Fr. 0.37</span></li>
+                    <li class="title">10’000<span>Fr. 0.30</span></li>
+                    <li class="title">28’000<span>Fr. 0.29</span></li>
                 </ul>
-                <p class="preiszusatz">Verpackung: Box à 50 Stück<br>Preise: in CHF, zuzüglich Porto und 7,7 % MWST</p>
+                <p class="preiszusatz">Verpackung: 5x 10 Stück in 50er Box, 1'000 Stück pro Karton<br>Preise: in CHF, zuzüglich Porto und 7,7 % MWST</p>
             </div>
         </article>
-        <article class="maskencontainer"> 
+        <article class="maskencontainer">
             <h1>Atemschutzmasken FFP-2/KN95</h1>
             <div class="twoicon">
                 <img src="img/alltagsmasken-covid-19-atemschutzmasken-ffp-2.jpg" alt="alltagsmasken-covid-19-atemschutzmasken-ffp-2" style="width:100%">
                 <h1 class="productb">Sofort lieferbar!</h1>
             </div>
-            <div class="twoicon r">            
+            <div class="twoicon r">
                 <p class="wichtig">Die 4-lagigen Masken der Schutzklasse FFP2/KN95 entsprechen der Norm EN 149:2001+A1:2009, sie sind CE- und FDA-zertifiziert.<br><br> Das Bundesamt für Gesundheit empfiehlt die Nutzung dieser Qualität für medizinisches Personal zum Eigenschutz vor Infektionen. Zudem eignet sie sich für Bauarbeiter, Lackierer und für Arbeiten mit Feinstaubbelastung.</p>
                 <p class="wichtig"><b>Bakterienfiltrationseffizienz BFE ≥95 %</b></p>
-                
+
                 <ul>
                     <li class="menge">Bestellmenge/Stück<span>Preis</span></li>
-                    <li class="title">100<span>Fr. 6.90</span></li>
-                    <li class="title">500<span>Fr. 6.30</span></li>
-                    <li class="title">2'500<span>Fr. 5.70</span></li>
+                    <li class="title">100<span>Fr. 1.90</span></li>
+                    <li class="title">500<span>Fr. 1.50</span></li>
+                    <li class="title">2'000<span>Fr. 1.40</span></li>
                 </ul>
-                <p class="preiszusatz">Verpackung: einzeln in Plastikbeutel eingeschweisst, Box à 50 Stück<br>Preise: in CHF, zuzüglich Porto und 7,7 % MWST</p>
+                <p class="preiszusatz">Verpackung: einzeln in Plastikbeutel eingeschweisst, Beutel à 25 Stück<br>Preise: in CHF, zuzüglich Porto und 7,7 % MWST</p>
             </div>
         </article>
     </section>
-    
-        
+
+
     <?php include 'footer.php'; ?>
     <?php include 'script.php'; ?>
 
